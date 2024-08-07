12b767c3835b32a651977cd007803fbfef29feb9
shaziaabdul <114333102+shaziaabdul@users.noreply.github.com>
2024-08-07 08:22:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+var jsxHeading = <h1 id="jsxHeading">Heading from JSX!!</h1>
+console.log(jsxHeading);
+rootReact.render(jsxHeading)
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+var header = <h1 id="header">Heading from JSX!!</h1>
+console.log(header);
+rootReact.render(header);
